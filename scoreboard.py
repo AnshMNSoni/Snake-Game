fea42ca6aa00358fbb176783d289fc58156949e7
AnshMNSoni <anshsoni702@gmail.com>
2024-06-10 10:17:20 +0530

    add high score tracer

--- scoreboard.py
+++ scoreboard.py
@@ -7,12 +7,14 @@ class ScoreBoard(Turtle):
     def __init__(self):
         self.count = 0
         self.high_count = 0
+        with open('high-score.txt') as data:
+            self.high_count = int(data.read())
+            
         super().__init__()
         self.penup()
         self.hideturtle()
         self.color('white')
         self.goto(0, 250)
-        # self.write(f'Score :: {self.count}', align=ALIGNMENT, font=FONT)
         self.write(f'Score :: {self.count} | High Score :: {self.high_count}', align=ALIGNMENT, font=FONT)
     
     def update(self):
@@ -22,7 +24,12 @@ class ScoreBoard(Turtle):
     def reset(self):
         if (self.count > self.high_count):
             self.high_count = self.count
+            
+            with open('high-score.txt', 'w') as h:
+                h.write(f"{self.high_count}")
+                
         self.count = 0
+        
     
     def increase_score(self):
         self.hideturtle()
