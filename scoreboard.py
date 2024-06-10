50d140b7e7fcd5df968143fd9fd7a8e912e83ff7
AnshMNSoni <anshsoni702@gmail.com>
2024-06-10 09:50:57 +0530

    added high score feature

--- scoreboard.py
+++ scoreboard.py
@@ -6,26 +6,26 @@ FONT = ('Courier', 22, 'normal')
 class ScoreBoard(Turtle):
     def __init__(self):
         self.count = 0
+        self.high_count = 0
         super().__init__()
         self.penup()
         self.hideturtle()
         self.color('white')
         self.goto(0, 250)
-        self.write(f'Score :: {self.count}', align=ALIGNMENT, font=FONT)
+        # self.write(f'Score :: {self.count}', align=ALIGNMENT, font=FONT)
+        self.write(f'Score :: {self.count} | High Score :: {self.high_count}', align=ALIGNMENT, font=FONT)
     
-    def gameover(self):
-        screen.clearscreen()
-        screen.bgcolor('black')
-        self.hideturtle()
-        self.goto(0, 250)
-        self.color('white')
-        self.write(f'Score :: {self.count}', align=ALIGNMENT, font=FONT)
-        self.goto(0, 0)
-        self.write(f':: Game Over ::', align=ALIGNMENT, font=FONT)
-        
-    def increase_score(self):
+    def update(self):
         self.clear()
+        self.write(f'Score :: {self.count} | High Score :: {self.high_count}', align=ALIGNMENT, font=FONT)
+        
+    def reset(self):
+        if (self.count > self.high_count):
+            self.high_count = self.count
+        self.count = 0
+    
+    def increase_score(self):
         self.hideturtle()
         self.count += 1
-        self.write(f'Score :: {self.count}', align=ALIGNMENT, font= FONT)
+        self.update()
         
